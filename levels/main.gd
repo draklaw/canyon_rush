9e0b6d4cac0f69d67baa5a6e7a0ba9210dbb55f7
Draklaw <drakdiv@gmail.com>
2020-04-18 21:58:05 +0200

    Add some shortcuts (escape quits, alt+enter toggle fullscreen).

---
+++ levels/main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+var enter_pressed: bool = false
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+
+	if Input.is_key_pressed(KEY_ENTER):
+		if not enter_pressed and Input.is_key_pressed(KEY_ALT):
+			OS.window_fullscreen = !OS.window_fullscreen
+		enter_pressed = true
+	else:
+		enter_pressed = false
