7c48a6c0ba30d5cf768ebad7d0d4d0d5b21ff76e
Draklaw <drakdiv@gmail.com>
2020-04-19 23:19:45 +0200

    More mob anims, again.

--- objects/mob/mob.gd
+++ objects/mob/mob.gd
@@ -26,7 +26,7 @@ const sprites = [
 	preload("res://objects/mob/masher_anims.tres"),
 	preload("res://objects/mob/fleer_anims.tres"),
 	preload("res://objects/mob/tanker_anims.tres"),
-	preload("res://objects/mob/masher_anims.tres"),
+	preload("res://objects/mob/dasher_anims.tres"),
 	preload("res://objects/mob/sniper_anims.tres"),
 ]
 
