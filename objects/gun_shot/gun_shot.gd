e9a0faf64c3621b6b5a9050b285c4d6273c7bd5f
Draklaw <drakdiv@gmail.com>
2020-04-20 21:08:02 +0200

    Add alien class (variant of mobs).

--- objects/gun_shot/gun_shot.gd
+++ objects/gun_shot/gun_shot.gd
@@ -5,6 +5,10 @@ enum { LAZOR, SPIT }
 export var type = LAZOR
 export var velocity: float = 1024
 export var damage: float = 10
+export var max_range: float = 3000
+
+
+var distance_traveled: float = 0
 
 
 func _ready() -> void:
@@ -14,6 +18,11 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	distance_traveled += delta * velocity
+	if distance_traveled > max_range:
+		print("PING")
+		queue_free()
+
 	var dir = transform.basis_xform(Vector2(1, 0))
 	position += dir * (delta * velocity)
 
